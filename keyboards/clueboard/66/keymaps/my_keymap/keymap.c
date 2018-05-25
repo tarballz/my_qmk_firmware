5c7d388b2c238582061f31ac916636f51b40e888
Payton Schwarz <pschwarz@mocana.com>
2018-05-25 16:32:40 -0700

    Add work layer

--- keyboards/clueboard/66/keymaps/my_keymap/keymap.c
+++ keyboards/clueboard/66/keymaps/my_keymap/keymap.c
@@ -7,8 +7,9 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 #define _BL 0
 #define _GL 1
-#define _FL 2
-#define _CL 3
+#define _WL 2
+#define _FL 3
+#define _CL 4
 
 // Declaring FN_CAPS to Momemtarily switch to _FL when held, and caps when tapped.
 #define FN_CAPS LT(_FL, KC_CAPSLOCK)
@@ -64,13 +65,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,	  KC_M,  KC_COMM, KC_DOT, KC_SLSH,KC_RO,  KC_RSFT,        KC_UP,           \
   KC_LCTL,KC_NO  ,KC_LALT,KC_MHEN,        KC_SPC, KC_SPC,                         KC_HENK,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
 
+
+  /* Keymap _WL: Work Layer
+   */
+[_WL] = KEYMAP(
+  KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_GRV, KC_BSPC,        KC_PGUP, \
+  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,                KC_PGDN, \
+  FN_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,                          \
+  KC_LSPO,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH,KC_RO,  KC_RSPC,        KC_UP,           \
+  KC_LCTL,KC_LGUI,KC_LALT,KC_MHEN,        KC_SPC, KC_SPC,                         KC_HENK,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
+
   /* Keymap _FL: Function Layer
    */
 [_FL] = KEYMAP(
   KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_DEL,         KC_VOLU, \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,                KC_VOLD, \
   _______,_______,MO(_CL),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                         \
-  _______,_______,_______,_______,_______,_______,_______,_______,TO(_BL),TO(_GL),_______,_______,_______,_______,        KC_PGUP,         \
+  _______,_______,_______,_______,_______,_______,_______,_______,TO(_BL),TO(_GL),TO(_WL),_______,_______,_______,        KC_PGUP,         \
   _______,_______,_______,_______,        _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
 
   /* Keymap _CL: Control layer
